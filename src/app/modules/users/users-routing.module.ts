bfd7411e459939cec169d6b321bda3cfc7cd30f7
MariiaHavlo <mariakolodiy01@gmail.com>
2022-05-25 16:32:38 +0300

    lesson 4

--- src/app/modules/users/users-routing.module.ts
+++ src/app/modules/users/users-routing.module.ts
@@ -4,9 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import {UsersComponent} from "./components/users/users.component";
 import {UserDetailsComponent} from "./components/user-details/user-details.component";
 import {UserResolver} from "./services";
+import {UserGuard} from "./guards";
 
 const routes: Routes = [
-  {path:'',component: UsersComponent,children:[
+  {path:'',component: UsersComponent, canActivate:[UserGuard], children:[
       {path:':id', component:UserDetailsComponent,  resolve: {userData:UserResolver}}
     ]}
 ];
